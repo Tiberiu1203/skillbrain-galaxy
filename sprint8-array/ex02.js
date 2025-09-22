8c1ab73b73a5fc6451a972966f195077a6dd56ab
Tiberiu Caraivan <angelo@caraivan.fr>
2025-09-22 17:00:54 +0200

    Am adaugat tema pentru array si loops

---
+++ sprint8-array/ex02.js
@@ -0,0 +1,31 @@
+// 2. Calculați suma numerelor dintr-un șir de numere
+
+// Vă puteți crea propriul șir de numere, dar luați în considerare să încercați această problemă cu câteva seturi diferite pentru a vă verifica soluția. De asemenea, puteți încerca să utilizați matrice de lungimi diferite.
+
+// Exemplu 1:
+
+// Input: [1, -5, 20, -34, 16, 29, 36, -4]
+
+// Output: 59
+
+// Exemplu 2:
+
+// Input: [3.45, -2.68, 356, -75.96, 64, 19.28]
+
+// Output: 364.09000000000003
+
+let numbers1 = [1, -5, 20, -34, 16, 29, 36, -4];
+let numbers2 = [3.45, -2.68, 356, -75.96, 64, 19.28];
+
+let sum1 = 0;
+let sum2 = 0;
+
+for (let i = 0; i < numbers1.length; i++) {
+  sum1 = sum1 + numbers1[i];
+}
+
+for (let i = 0; i < numbers2.length; i++) {
+  sum2 = sum2 + numbers2[i];
+}
+
+console.log("Suma listei 1 este:", sum1, " | Suma listei 2 este:", sum2);
